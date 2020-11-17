48e33a6bac87962ff78139e711eda34ec4362ba1
Faralaks <Faralaks@ya.ru>
2020-11-17 12:52:38 +0300

    Унифицирован когфигурационный файл

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from os import path, getcwd
 DEBUG = True
 PORT = 5000
 HOST = "0.0.0.0"
-DB_PATH = "./db/db.sqlite"
-SECRET_KEY = b'Ni5t~Igd51Mp7'
+DB_PATH = path.join(getcwd(), 'db', 'db.sqlite')
+SECRET_KEY = 'secret'
 TEMPLATES_AUTO_RELOAD = True
-LOGIN = "кисик"
-PAS = r'Ci~eK*{{Zdp@%DiRGZ@VTg*X4x#Cyf'
+LOGIN = "login"
+PAS = 'pas'
 COOKIE_SECURE = 'Secure'
 COOKIE_DURATION = timedelta(days=5)
